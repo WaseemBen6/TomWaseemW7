cd0e3d0b4c157e46e6fb7284f202648ba5f97932
Mitchell Colson <mitch2103@hotmail.co.uk>
2021-09-10 15:23:59 +0100

    reshuffled input elements added styling

--- src/components/Cities/Index.js
+++ src/components/Cities/Index.js
@@ -6,13 +6,15 @@ const Cities = ({ data }) => {
       <div id="sunrise" class="suntime">
         <h2>Sunrise</h2>
         <p id="sunriseTime">{data.sunrise}</p>
+        <img src="" alt="" />
+      </div>
+      <div id="dayLength">
+        <p>Length of the day: {data.day_length}</p>
       </div>
       <div id="sunset" class="suntime">
         <h2>Sunset</h2>
         <p id="sunsetTime">{data.sunset}</p>
-      </div>
-      <div id="dayLength">
-        <p>Length of the day: {data.day_length}</p>
+        <img src="" alt="" />
       </div>
     </section>
   );
