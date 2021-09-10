f890312419a963ca60851061499ed5bc8291c5f5
Mitchell Colson <mitch2103@hotmail.co.uk>
2021-09-10 16:00:15 +0100

    added styling and images

--- src/components/Cities/Index.js
+++ src/components/Cities/Index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import sunset from "../../sunset2.png";
+import sunrise from "../../sunrise2.png";
 
 const Cities = ({ data }) => {
   return (
-    <>
+    <div id="citiesText">
       <div id="solarnoon" class="suntime">
         <h2>Solar noon</h2>
         <p id="solarnoontime">{data.solar_noon}</p>
@@ -12,7 +14,7 @@ const Cities = ({ data }) => {
         <div id="sunrise" class="suntime">
           <h2>Sunrise</h2>
           <p id="sunriseTime">{data.sunrise}</p>
-          <img src="" alt="" />
+          <img src={sunrise} alt="sunrise" />
         </div>
         <div id="dayLength">
           <p>Length of the day: {data.day_length}</p>
@@ -20,10 +22,10 @@ const Cities = ({ data }) => {
         <div id="sunset" class="suntime">
           <h2>Sunset</h2>
           <p id="sunsetTime">{data.sunset}</p>
-          <img src="" alt="" />
+          <img src={sunset} alt="sunset" />
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
