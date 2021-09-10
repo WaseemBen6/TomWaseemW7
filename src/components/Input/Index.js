57dd1a02989f0c369c0b5ceda30bdd9f3ab749d4
Waseem <waseembenaras@gmail.com>
2021-09-10 15:40:25 +0100

    added to list

--- src/components/Input/Index.js
+++ src/components/Input/Index.js
@@ -3,7 +3,10 @@ import React from "react";
 const InputLocation = ({ onChange }) => (
   <select onChange={onChange} name="Cities" id="City">
     <option value="Birmingham">Birmingham</option>
+    <option value="Florida">Florida</option>
     <option value="London">London</option>
+    <option value="Moscow">Moscow</option>
+    <option value="Paris">Paris</option>
     <option value="Sydney">Sydney</option>
   </select>
 );
