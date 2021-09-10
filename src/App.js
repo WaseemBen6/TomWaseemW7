05753a2331eb33460105c2e0985da52afdfdb340
Waseem <waseembenaras@gmail.com>
2021-09-10 12:26:26 +0100

    Cities component added and async function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputLocation from "./components/Input/Index";
+
 import "./App.css";
+import Cities from "./components/Cities/Index";
+
+let apiKey = "a0eac15a53f146068bfb3d203085ee96"
 
 function App() {
   const [location, setLocation] = useState("Birmingham");
+  const [astronomyData, setAstronomyData] = useState({});
+
 
   function changeLocationState(e) {
     setLocation(e.target.value);
     console.log(location);
   }
 
+  useEffect(()=>{
+    async function getData() {
+      const res = await fetch(`https://api.ipgeolocation.io/astronomy?apiKey=${apiKey}&location=${location}`
+      )
+      const data = await res.json();
+      setAstronomyData(data)
+      console.log(astronomyData)
+    }
+    getData()
+  },[location])
+
+
   return (
     <>
       <InputLocation onChange={changeLocationState} />
+      <Cities data={astronomyData}/>
     </>
   );
 }
--- src/components/Cities/Index.js
+++ src/components/Cities/Index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Cities = () => (
- 
-);
+const Cities = ({ data }) => {
+ return (
+ <section>
+ <h2>Sunrise</h2>
+    <p>{data.sunrise}</p>
+ <h2>Sunset</h2>
+    <p>{data.sunset}</p>
+ </section>
+)};
 
-Cities.propTypes = {};
-
-Cities.defaultProps = {};
 
 export default Cities;
